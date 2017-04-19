a72de66708f996f5fb2e4b23a8ce7ac7587b361c
artur <Artur-Mamedbekov@yandex.ru>
2017-04-19 18:16:58 +0300

    Вынес обработчик ошибки плагина error в ключ handler контейнера

--- _plugins/error.php
+++ _plugins/error.php
@@ -3,14 +3,12 @@ function error_default_handler($error, $content, array $sp){
   return var_export($error, true);
 }
 
+isset($sp['error']['handler']) || $sp['error']['handler'] = 'error_default_handler';
+
 middleware_add(function($content, array $next) use($sp){
   $error = error_get_last();
   if(is_array($error)){
-    if(!isset($sp['error'])){
-      $sp['error'] = 'error_default_handler';
-    }
-
-    return call_user_func($sp['error'], $error, $content, $sp);
+    return call_user_func($sp['error']['handler'], $error, $content, $sp);
   }
 
   return middleware_next($content, $next);
