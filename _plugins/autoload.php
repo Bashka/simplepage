3c7229fe838ff5d9c09c5470272c7568b371a6e2
artur <Artur-Mamedbekov@yandex.ru>
2017-04-20 13:08:10 +0300

    Расширил плагин autoload загрузкой класса по прямой ссылке

--- _plugins/autoload.php
+++ _plugins/autoload.php
@@ -1,10 +1,18 @@
 <?php
+isset($sp['autoload']['map']) || $sp['autoload']['map'] = [];
+isset($sp['autoload']['psr4']) || $sp['autoload']['psr4'] = __DIR__ . '/../_autoload';
+
 spl_autoload_register(function($class) use($sp){
-  isset($sp['autoload']) || $sp['autoload'] = __DIR__ . '/../_autoload';
-  foreach((array) $sp['autoload'] as $dir){
-    $path = sprintf('%s/%s.php', $dir, str_replace('\\', '/', $class));
-    if(is_readable($path)){
-      include($path);
+  if(isset($sp['autoload']['map'][$class])){
+    include($sp['autoload']['map'][$class]);
+  }
+  else{
+    foreach((array) $sp['autoload']['psr4'] as $dir){
+      $path = sprintf('%s/%s.php', $dir, str_replace('\\', '/', $class));
+      if(is_readable($path)){
+        include($path);
+        break;
+      }
     }
   }
 });
